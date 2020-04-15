ce80dc25dc4e9d8bc9fd610c057a22979b24699b
JJ Merelo <jjmerelo@gmail.com>
2020-04-15 11:42:34 +0200

    Improves styles

--- moving-target-evostar-2019.R
+++ moving-target-evostar-2019.R
@@ -6,9 +6,10 @@ data <- read.csv("results/all_results.csv")
 
 ## ----results-time,cache=FALSE,echo=FALSE--------------------------------------
 ggplot(data,aes(x=Population,y=Days,color=Web)) + geom_tufteboxplot()+ theme_tufte()
+ggsave("duration.png",width=12,height=8)
 
 ggplot(data,aes(x=Population,y=Copies,color=Web)) + geom_point()+geom_jitter()+ theme_tufte()
-
+ggsave("copies.png",width=12,height=8)
 
 
 ## ----results-static,cache=FALSE,echo=FALSE------------------------------------
@@ -16,10 +17,11 @@ ggplot(data,aes(x=Population,y=Copies,color=Web)) + geom_point()+geom_jitter()+ 
 data$Population <- as.factor(data$Population)
 data.web <- data[data$Web == "Static",]
 ggplot(data.web, aes(x=Population,y=Fitness,color=Crossover,shape=Mutation, size=Copies*10,alpha=Days)) + geom_jitter(width = 0.3, height=0)+ theme_tufte()
+ggsave("crossover-mutation.png",width=12,height=8)
 
 
 ## ----results-juice-shop,cache=FALSE,echo=FALSE--------------------------------
 data.juice <- data[data$Web == "Juice Shop",]
 
 ggplot(data.juice, aes(x=Population,y=Fitness,color=Crossover,shape=Mutation, size=Copies*10,alpha=Days))+ geom_jitter(width = 0.3,height=0) +  theme_tufte()
-
+ggsave("crossover-mutation-juice-shop.png",width=12,height=8)
